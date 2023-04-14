f386780b38be10502dba9df6173c24324270b21c
Tommy1111112 <76492524+Tommy1111112@users.noreply.github.com>
2023-04-14 09:51:18 +0100

    Update orion.lua

--- orion.lua
+++ orion.lua
@@ -16,8 +16,8 @@ local OrionLib = {
 		Default = {
 			Main = Color3.fromRGB(15, 15, 15),
 			Second = Color3.fromRGB(15, 15, 15),
-			Stroke = Color3.fromRGB(55,55,192),
-			Divider = Color3.fromRGB(55,55,192),
+			Stroke = Color3.fromRGB(192,55,55),
+			Divider = Color3.fromRGB(192,55,55),
 			Text = Color3.fromRGB(240, 240, 240),
 			TextDark = Color3.fromRGB(180, 180, 180)
 		} -- 192 55 55
